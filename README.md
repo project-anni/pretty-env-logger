a2cf8f29dce9d45609cb339e32190b1ec213a98d
Yesterday17 <mmf@mmf.moe>
2021-08-19 16:08:26 +0800

    Remove useless parts & expose formatter directly.
    Instead of initializing `env_logger` directly, the change makes `pretty_env_logger` available in a normal `env_logger` builder. You can use `env_logger::builder().format(pretty_env_logger).init()` to initialize env_logger with all addition utilties provided by `env_logger` builder.

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # pretty-env-logger
 
-[![Crates.io](https://img.shields.io/crates/v/pretty_env_logger.svg)](https://crates.io/crates/pretty_env_logger)
-[![Docs](https://docs.rs/pretty_env_logger/badge.svg)](https://docs.rs/pretty_env_logger)
-[![MIT/APACHE-2.0](https://img.shields.io/crates/l/pretty_env_logger.svg)](https://crates.io/crates/pretty_env_logger)
-[![Travis CI](https://travis-ci.org/seanmonstar/pretty-env-logger.svg?branch=master)](https://travis-ci.org/seanmonstar/pretty-env-logger)
-
 A simple logger built on top of [env_logger](https://docs.rs/env_logger).
 It is configured via an environment variable and writes to standard
 error with nice colored output for log levels.
@@ -18,7 +13,7 @@ Add the dependency to your `Cargo.toml`:
 ```toml
 [dependencies]
 log = "0.4"
-pretty_env_logger = "0.4"
+pretty_env_logger = { git = "https://github.com/project-anni/pretty_env_logger" }
 ```
 
 Add some usage to your application:
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,4 @@
 #![cfg_attr(test, deny(warnings))]
-#![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/pretty_env_logger/0.4.0")]
-
-//! A logger configured via an environment variable which writes to standard
-//! error with nice colored output for log levels.
-//!
-//! ## Example
-//!
-//! ```
-//! extern crate pretty_env_logger;
-//! #[macro_use] extern crate log;
-//!
-//! fn main() {
-//!     pretty_env_logger::init();
-//!
-//!     trace!("a trace example");
-//!     debug!("deboogging");
-//!     info!("such information");
-//!     warn!("o_O");
-//!     error!("boom");
-//! }
-//! ```
-//!
-//! Run the program with the environment variable `RUST_LOG=trace`.
-//!
-//! ## Defaults
-//!
-//! The defaults can be setup by calling `init()` or `try_init()` at the start
-//! of the program.
-//!
-//! ## Enable logging
-//!
-//! This crate uses [env_logger][] internally, so the same ways of enabling
-//! logs through an environment variable are supported.
-//!
-//! [env_logger]: https://docs.rs/env_logger
 
 #[doc(hidden)]
 pub extern crate env_logger;
@@ -44,183 +8,70 @@ extern crate log;
 use std::fmt;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use env_logger::{fmt::{Color, Style, StyledValue}, Builder};
-use log::Level;
+use env_logger::fmt::{Color, Style, StyledValue, Formatter};
+use log::{Level, Record};
 
-
-/// Initializes the global logger with a pretty env logger.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Panics
-///
-/// This function fails to set the global logger if one has already been set.
 pub fn init() {
-    try_init().unwrap();
+    env_logger::builder().format(formatter).init();
 }
 
-/// Initializes the global logger with a timed pretty env logger.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Panics
-///
-/// This function fails to set the global logger if one has already been set.
 pub fn init_timed() {
-    try_init_timed().unwrap();
+    env_logger::builder().format(timed_formatter).init();
 }
 
-/// Initializes the global logger with a pretty env logger.
+/// Formatter used in env_logger builder
 ///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
-pub fn try_init() -> Result<(), log::SetLoggerError> {
-    try_init_custom_env("RUST_LOG")
-}
+/// Formats output with colored level
+pub fn formatter(f: &mut Formatter, record: &Record) -> std::io::Result<()> {
+    use std::io::Write;
+    let target = record.target();
+    let max_width = max_target_width(target);
 
-/// Initializes the global logger with a timed pretty env logger.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
-pub fn try_init_timed() -> Result<(), log::SetLoggerError> {
-    try_init_timed_custom_env("RUST_LOG")
-}
+    let mut style = f.style();
+    let level = colored_level(&mut style, record.level());
 
-/// Initialized the global logger with a pretty env logger, with a custom variable name.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Panics
-///
-/// This function fails to set the global logger if one has already been set.
-pub fn init_custom_env(environment_variable_name: &str) {
-    try_init_custom_env(environment_variable_name).unwrap();
-}
-
-/// Initialized the global logger with a pretty env logger, with a custom variable name.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
-pub fn try_init_custom_env(environment_variable_name: &str) -> Result<(), log::SetLoggerError> {
-    let mut builder = formatted_builder();
-
-    if let Ok(s) = ::std::env::var(environment_variable_name) {
-        builder.parse_filters(&s);
-    }
-
-    builder.try_init()
-}
-
-/// Initialized the global logger with a timed pretty env logger, with a custom variable name.
-///
-/// This should be called early in the execution of a Rust program, and the
-/// global logger may only be initialized once. Future initialization attempts
-/// will return an error.
-///
-/// # Errors
-///
-/// This function fails to set the global logger if one has already been set.
-pub fn try_init_timed_custom_env(environment_variable_name: &str) -> Result<(), log::SetLoggerError> {
-    let mut builder = formatted_timed_builder();
-
-    if let Ok(s) = ::std::env::var(environment_variable_name) {
-        builder.parse_filters(&s);
-    }
-
-    builder.try_init()
-}
-
-/// Returns a `env_logger::Builder` for further customization.
-///
-/// This method will return a colored and formatted `env_logger::Builder`
-/// for further customization. Refer to env_logger::Build crate documentation
-/// for further details and usage.
-pub fn formatted_builder() -> Builder {
-    let mut builder = Builder::new();
-
-    builder.format(|f, record| {
-        use std::io::Write;
-
-        let target = record.target();
-        let max_width = max_target_width(target);
-
-        let mut style = f.style();
-        let level = colored_level(&mut style, record.level());
-
-        let mut style = f.style();
-        let target = style.set_bold(true).value(Padded {
-            value: target,
-            width: max_width,
-        });
-
-        writeln!(
-            f,
-            " {} {} > {}",
-            level,
-            target,
-            record.args(),
-        )
+    let mut style = f.style();
+    let target = style.set_bold(true).value(Padded {
+        value: target,
+        width: max_width,
     });
 
-    builder
+    writeln!(
+        f,
+        " {} {} > {}",
+        level,
+        target,
+        record.args(),
+    )
 }
 
-/// Returns a `env_logger::Builder` for further customization.
+/// Formatter used in env_logger builder
 ///
-/// This method will return a colored and time formatted `env_logger::Builder`
-/// for further customization. Refer to env_logger::Build crate documentation
-/// for further details and usage.
-pub fn formatted_timed_builder() -> Builder {
-    let mut builder = Builder::new();
+/// Formats output with time and colored level
+pub fn timed_formatter(f: &mut Formatter, record: &Record) -> std::io::Result<()> {
+    use std::io::Write;
+    let target = record.target();
+    let max_width = max_target_width(target);
 
-    builder.format(|f, record| {
-        use std::io::Write;
-        let target = record.target();
-        let max_width = max_target_width(target);
+    let mut style = f.style();
+    let level = colored_level(&mut style, record.level());
 
-        let mut style = f.style();
-        let level = colored_level(&mut style, record.level());
-
-        let mut style = f.style();
-        let target = style.set_bold(true).value(Padded {
-            value: target,
-            width: max_width,
-        });
-
-        let time = f.timestamp_millis();
-
-        writeln!(
-            f,
-            " {} {} {} > {}",
-            time,
-            level,
-            target,
-            record.args(),
-        )
+    let mut style = f.style();
+    let target = style.set_bold(true).value(Padded {
+        value: target,
+        width: max_width,
     });
 
-    builder
+    let time = f.timestamp_millis();
+
+    writeln!(
+        f,
+        " {} {} {} > {}",
+        time,
+        level,
+        target,
+        record.args(),
+    )
 }
 
 struct Padded<T> {
@@ -230,7 +81,7 @@ struct Padded<T> {
 
 impl<T: fmt::Display> fmt::Display for Padded<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{: <width$}", self.value, width=self.width)
+        write!(f, "{: <width$}", self.value, width = self.width)
     }
 }
 
